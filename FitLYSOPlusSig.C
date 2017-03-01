aeaa204b83f438b41e9c40ed5ba9f3003871fd05
Busato <busato@in2p3.fr>
2017-03-01 18:24:33 +0100

    update

--- FitLYSOPlusSig.C
+++ FitLYSOPlusSig.C
@@ -1,14 +1,7 @@
-#ifndef __CINT__
-#include "RooGlobalFunc.h"
-#endif
 #include "RooRealVar.h"
 #include "RooDataSet.h"
 #include "RooGaussian.h"
-#include "RooConstVar.h"
-#include "RooPolynomial.h"
 #include "RooHistPdf.h"
-#include "TCanvas.h"
-#include "TAxis.h"
 #include "RooPlot.h"
 using namespace RooFit ;
 
@@ -23,11 +16,13 @@ void FitLYSOPlusSig()
   RooDataSet* data = new RooDataSet("data", "data_LYSOPlusSig", t, E);
   RooDataHist* hist = data->binnedClone();
 
+  cout << "no entries in dataset = " << data->sumEntries() << endl;
+  
   TFile* f_LYSO = new TFile("data/run78.root");
   TTree* t_LYSO = (TTree*) f_LYSO->Get("tree");
   RooDataSet* data_LYSO = new RooDataSet("data_LYSO", "data_LYSO", t_LYSO, E);
   RooDataHist* hist_LYSO = data_LYSO->binnedClone();
-  RooHistPdf* histpdf_LYSO = new RooHistPdf("histpdf_LYSO","histpdf_LYSO",E,*hist_LYSO,0) ;
+  RooHistPdf* histpdf_LYSO = new RooHistPdf("histpdf_LYSO","histpdf_LYSO",E,*hist_LYSO,0);
   RooRealVar lyso_yield("lyso_yield", "yield of lyso", 100, 0, 1000000);
   
   RooRealVar sig_peak_mean("sig_peak_mean", "mean of gaussian for signal peak", 511, 420, 610, "keV");
@@ -41,12 +36,13 @@ void FitLYSOPlusSig()
   shapes.add(sig_gaussian);  yields.add(sig_yield);
   RooAddPdf totalPdf("totalPdf", "sum of signal and background PDF's", shapes, yields);
 
-  totalPdf.fitTo(*data);
+  totalPdf.fitTo(*data, Extended());
   
   // Plot unbinned data and histogram pdf overlaid
   RooPlot* frame = E.frame(Bins(100)) ;
   data->plotOn(frame) ;
   totalPdf.plotOn(frame);
+  //histpdf_LYSO->plotOn(frame);
   frame->Draw();
 
 }
