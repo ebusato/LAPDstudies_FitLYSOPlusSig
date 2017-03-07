0acd27f5c007f9fd392503474259454c29372f44
Busato <busato@in2p3.fr>
2017-03-07 16:47:47 +0100

    update

--- FitLYSOPlusSig.C
+++ FitLYSOPlusSig.C
@@ -6,59 +6,67 @@
 using namespace RooFit ;
 
 
-RooDataHist* GetDataHistFromTH1(TTree* t, RooRealVar& var, string TH1Name, string histName)
+RooDataHist* GetDataHistFromTH1(TTree* t, RooRealVar* var, string TH1Name, string histName)
 {
   //RooDataSet* data = new RooDataSet("data", "data", t, var);
   //RooDataHist* hist = data->binnedClone();
   //return hist
-	
+
   TH1* h = new TH1F(TH1Name.c_str(), TH1Name.c_str(), 200, 0, 1200);
   TString s("E>>");
   s += TH1Name.c_str();
-  t->Draw(s.Data());
-  return new RooDataHist(histName.c_str(), histName.c_str(), var, Import(*h)) ;
+  t->Draw(s.Data(), "NoPulses == 2 && Sat == 0");
+  return new RooDataHist(histName.c_str(), histName.c_str(), *var, Import(*h)) ;
 }
 
-void FitLYSOPlusSig(string dataFile, string lysoFile)
+RooAddPdf* MakeModel(RooRealVar* E, TTree* t_LYSO)
 {
-  RooRealVar E("E", "Energy", 0, 1200, "keV");
-  E.setBins(100);
+  RooDataHist* hist_LYSO = GetDataHistFromTH1(t_LYSO, E, "hE_lyso", "hist_lyso");
+  cout << "no entries in RooDataHist LYSO = " << hist_LYSO->sum(false) << endl;
+  RooHistPdf* histpdf_LYSO = new RooHistPdf("histpdf_LYSO","histpdf_LYSO", *E,*hist_LYSO,0);
+  RooRealVar* lyso_yield = new RooRealVar("lyso_yield", "yield of lyso", 100, 0, 1000000);
+  
+  RooRealVar* sig_peak_mean = new RooRealVar("sig_peak_mean", "mean of gaussian for signal peak", 511, 420, 610, "keV");
+  RooRealVar* sig_peak_sigma = new RooRealVar("sig_peak_sigma", "width of gaussian for signal peak", 40, 0, 90, "keV");
+  RooGaussian* sig_gaussian = new RooGaussian("sig_gaussian", "gaussian for signal peak", *E, *sig_peak_mean, *sig_peak_sigma);
+  RooRealVar* sig_yield = new RooRealVar("sig_yield", "yield signal peak", 1000, 0, 1000000);
+  
+  RooArgList* shapes = new RooArgList();
+  RooArgList* yields = new RooArgList();
+  shapes->add(*histpdf_LYSO); yields->add(*lyso_yield);
+  shapes->add(*sig_gaussian);  yields->add(*sig_yield);
+  RooAddPdf* model = new RooAddPdf("totalPdf", "sum of signal and background PDF's", *shapes, *yields);
+  return model;
+}
+
+RooFitResult* FitLYSOPlusSig(string dataFile, string lysoFile)
+{
+  RooRealVar* E = new RooRealVar("E", "Energy", 0, 1200, "keV");
+  E->setBins(100);
+    
+  TFile* f_LYSO = new TFile(lysoFile.c_str());
+  TTree* t_LYSO = (TTree*) f_LYSO->Get("tree");
+  RooAddPdf* model = MakeModel(E, t_LYSO);
   
   TFile* f = new TFile(dataFile.c_str());
   TTree* t = (TTree*) f->Get("tree");
   int noEntries = t->GetEntries();
-  
-  TFile* f_LYSO = new TFile(lysoFile.c_str());
-  TTree* t_LYSO = (TTree*) f_LYSO->Get("tree");
-  
 
   RooDataHist* hist = GetDataHistFromTH1(t, E, "hE_data", "hist_data");
   cout << "no entries in RooDataHist data = " << hist->sum(false) << endl;
- 
-  RooDataHist* hist_LYSO = GetDataHistFromTH1(t_LYSO, E, "hE_lyso", "hist_lyso");
-  cout << "no entries in RooDataHist LYSO = " << hist_LYSO->sum(false) << endl;
-  RooHistPdf* histpdf_LYSO = new RooHistPdf("histpdf_LYSO","histpdf_LYSO",E,*hist_LYSO,0);
-  RooRealVar lyso_yield("lyso_yield", "yield of lyso", 100, 0, 1000000);
-  
-  RooRealVar sig_peak_mean("sig_peak_mean", "mean of gaussian for signal peak", 511, 420, 610, "keV");
-  RooRealVar sig_peak_sigma("sig_peak_sigma", "width of gaussian for signal peak", 40, 0, 90, "keV");
-  RooGaussian sig_gaussian("sig_gaussian", "gaussian for signal peak", E, sig_peak_mean, sig_peak_sigma);
-  RooRealVar sig_yield("sig_yield", "yield signal peak", 1000, 0, 1000000);
-  
-  RooArgList shapes;
-  RooArgList yields;
-  shapes.add(*histpdf_LYSO); yields.add(lyso_yield);
-  shapes.add(sig_gaussian);  yields.add(sig_yield);
-  RooAddPdf totalPdf("totalPdf", "sum of signal and background PDF's", shapes, yields);
 
-  totalPdf.fitTo(*hist, Extended());
+
+  E->setRange("whole", 0, 1200);
+  E->setRange("betaContinuum", 700, 1200);
+  
+  RooFitResult* fitRes = model->fitTo(*hist, Extended()); //, Range("betaContinuum"));
   // Plot unbinned data and histogram pdf overlaid
-  RooPlot* frame = E.frame(Bins(100)) ;
-  hist->plotOn(frame, DrawOption("PX")) ;
-  totalPdf.plotOn(frame);
-  totalPdf.plotOn(frame, Components("sig_gaussian"),LineColor(kRed));
-  totalPdf.plotOn(frame, Components("histpdf_LYSO"),LineColor(kGreen+2));
-  //histpdf_LYSO->plotOn(frame);
+  RooPlot* frame = E->frame(Bins(100)) ;
+  hist->plotOn(frame, DrawOption("PX"));
+  model->plotOn(frame, Range("whole"));
+  model->plotOn(frame, Range("whole"), Components("sig_gaussian"),LineColor(kRed));
+  model->plotOn(frame, Range("whole"), Components("histpdf_LYSO"),LineColor(kGreen+2));
+  hist->plotOn(frame) ;
   frame->Draw();
   
  // TH1* hh_1d = (TH1*) totalPdf->createHistogram("mean,sigma_g2,frac",25,25,25);
@@ -68,6 +76,9 @@ void FitLYSOPlusSig(string dataFile, string lysoFile)
   PutText(0.7, 0.85-yShift, kBlack, "LPC");
   PutText(0.7, 0.85-2*yShift, kBlack, "Na22 (16 kBq)");
   
+  return 0;
+  
+  /*
   E.setRange("signal_window", 420, 600) ;
   
   RooAbsReal* igx_sig = sig_gaussian.createIntegral(E,NormSet(E),Range("signal_window")) ;
@@ -76,8 +87,8 @@ void FitLYSOPlusSig(string dataFile, string lysoFile)
   double int_lyso_window = igx_lyso->getVal();
   double yield_sig_window = int_sig_window * sig_yield.getVal();
   double yield_lyso_window = int_lyso_window * lyso_yield.getVal();
-  cout << "int[E|signal]_Norm[E] = " << int_sig_window << endl ;
-  cout << "int[E|LYSO]_Norm[E] = " << int_lyso_window << endl ;
+  cout << "int[E|signal]_Norm[E] = " << int_sig_window << endl;
+  cout << "int[E|LYSO]_Norm[E] = " << int_lyso_window << endl;
   cout << "sig in window = "<< yield_sig_window << endl;
   cout << "lyso in window = "<< yield_lyso_window << endl;
   cout << "s/sqrt(b) = " << yield_sig_window / sqrt(yield_lyso_window) << endl;
@@ -106,9 +117,11 @@ void FitLYSOPlusSig(string dataFile, string lysoFile)
   cout << "N1prime = " << N1prime << endl;
   cout << "N2prime = " << N2prime << endl;
   cout << "s/sqrt(b) = "<< N2prime/sqrt(N1prime) << endl;
+  */
 }
 
 void FitLYSOPlusSig()
 {
-  FitLYSOPlusSig("data/run63.root", "data/run78.root");
+  FitLYSOPlusSig("~/godaq_rootfiles/analysis_v2.10.0/run67.root", "~/godaq_rootfiles/analysis_v2.10.0/run79.root");
+  //FitLYSOPlusSig("~/godaq_rootfiles/analysis_v2.10.0/run63.root", "~/godaq_rootfiles/analysis_v2.10.0/run78.root");
 }
