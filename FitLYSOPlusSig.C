f03791273deaa5b3b9c0364939968f1276451d6c
Busato <busato@in2p3.fr>
2017-03-08 11:01:29 +0100

    update

--- FitLYSOPlusSig.C
+++ FitLYSOPlusSig.C
@@ -12,20 +12,18 @@ RooDataHist* GetDataHistFromTH1(TTree* t, RooRealVar* var, string TH1Name, strin
   //RooDataHist* hist = data->binnedClone();
   //return hist
 
-  TH1* h = new TH1F(TH1Name.c_str(), TH1Name.c_str(), 200, 0, 1200);
+  TH1* h = new TH1F(TH1Name.c_str(), TH1Name.c_str(), var->getBinning().numBins(), var->getMin(), var->getMax());
   TString s("E[0]>>");
   s += TH1Name.c_str();
-  t->Draw(s.Data(), "");
+  t->Draw(s.Data(), "", "goff");
   return new RooDataHist(histName.c_str(), histName.c_str(), *var, Import(*h)) ;
 }
 
-RooAddPdf* MakeModel(RooRealVar* E, TTree* t_LYSO)
+RooAddPdf* MakeModel(RooRealVar* E, RooDataHist* hist_LYSO)
 {
-  RooDataHist* hist_LYSO = GetDataHistFromTH1(t_LYSO, E, "hE_lyso", "hist_lyso");
-  cout << "no entries in RooDataHist LYSO = " << hist_LYSO->sum(false) << endl;
   RooHistPdf* histpdf_LYSO = new RooHistPdf("histpdf_LYSO","histpdf_LYSO", *E,*hist_LYSO,0);
   RooRealVar* lyso_yield = new RooRealVar("lyso_yield", "yield of lyso", 100000, 0, 1000000);
-  
+ 
   RooRealVar* sig_peak_mean = new RooRealVar("sig_peak_mean", "mean of gaussian for signal peak", 511, 420, 610, "keV");
   RooRealVar* sig_peak_sigma = new RooRealVar("sig_peak_sigma", "width of gaussian for signal peak", 40, 0, 90, "keV");
   RooGaussian* sig_gaussian = new RooGaussian("sig_gaussian", "gaussian for signal peak", *E, *sig_peak_mean, *sig_peak_sigma);
@@ -39,21 +37,40 @@ RooAddPdf* MakeModel(RooRealVar* E, TTree* t_LYSO)
   return model;
 }
 
-void MakeCalculationsSovSqrtB(RooAddPdf* model, RooRealVar* E, int noEntries)
+void MakeCalculationsSensitivity(RooDataHist* hist_LYSO, RooAddPdf* model, RooRealVar* E, int noEntries)
 {
   RooAbsPdf* sig_gaussian = (RooAbsPdf*) model->pdfList().find("sig_gaussian");
-  RooAbsPdf* histpdf_LYSO = (RooAbsPdf*) model->pdfList().find("histpdf_LYSO");
-  RooAbsReal* igx_sig = sig_gaussian->createIntegral(*E,NormSet(*E),Range("signal_window")) ;
-  RooAbsReal* igx_lyso = histpdf_LYSO->createIntegral(*E,NormSet(*E),Range("signal_window")) ;
-  double int_sig_window = igx_sig->getVal();
-  double int_lyso_window = igx_lyso->getVal();
-  
   RooAbsReal* sig_yield = (RooAbsReal*) model->coefList().find("sig_yield");
   RooAbsReal* lyso_yield = (RooAbsReal*) model->coefList().find("lyso_yield");
-  double yield_sig_window = int_sig_window * sig_yield->getVal();
-  double yield_lyso_window = int_lyso_window * lyso_yield->getVal();
+  
+  // OTH stuff
+  TCanvas* c2 = new TCanvas("c2","c2",800,400);
+  c2->Divide(2,1);
+  c2->cd(1);
+  TH1F* hLYSO_gen = (TH1F*) hist_LYSO->createHistogram("E");
+  hLYSO_gen->Sumw2();
+  hLYSO_gen->Scale(lyso_yield->getVal()/hLYSO_gen->Integral());
+  hLYSO_gen->Draw("e");
+  cout << "ERROR=" << hLYSO_gen->GetEntries() << "  " << hLYSO_gen->Integral() << "  " << hLYSO_gen->GetBinContent(hLYSO_gen->GetXaxis()->FindBin(400)) << " " << 
+hLYSO_gen->GetBinError(hLYSO_gen->GetXaxis()->FindBin(400)) << endl;
+  c2->cd(2);
+  RooDataHist* dhSig_gen = sig_gaussian->generateBinned(*E, sig_yield->getVal(), ExpectedData());
+  TH1F* hSig_gen = (TH1F*) dhSig_gen->createHistogram("E");
+  hSig_gen->Draw("e");
+  cout << "hSig_gen: " << hSig_gen->Integral() << endl;
+  
+  RooAbsPdf* histpdf_LYSO = (RooAbsPdf*) model->pdfList().find("histpdf_LYSO");
+  // Analytical stuff
+  E->setRange("signalWindow", 420, 600) ;
+  RooAbsReal* igx_sig = sig_gaussian->createIntegral(*E,NormSet(*E),Range("signalWindow")) ;
+  RooAbsReal* igx_lyso = histpdf_LYSO->createIntegral(*E,NormSet(*E),Range("signalWindow")) ;
+  double int_sig_window = igx_sig->getVal();
+  double int_lyso_window = igx_lyso->getVal();
   cout << "int[E|signal]_Norm[E] = " << int_sig_window << endl;
   cout << "int[E|LYSO]_Norm[E] = " << int_lyso_window << endl;
+  
+  double yield_sig_window = int_sig_window * sig_yield->getVal();
+  double yield_lyso_window = int_lyso_window * lyso_yield->getVal();
   cout << "sig in window = "<< yield_sig_window << endl;
   cout << "lyso in window = "<< yield_lyso_window << endl;
   cout << "s/sqrt(b) = " << yield_sig_window / sqrt(yield_lyso_window) << endl;
@@ -82,43 +99,44 @@ void MakeCalculationsSovSqrtB(RooAddPdf* model, RooRealVar* E, int noEntries)
   cout << "N1prime = " << N1prime << endl;
   cout << "N2prime = " << N2prime << endl;
   cout << "s/sqrt(b) = "<< N2prime/sqrt(N1prime) << endl;
+ 
 }
 
 RooFitResult* FitLYSOPlusSig(string dataFile, string lysoFile)
 {
   RooRealVar* E = new RooRealVar("E", "Energy", 0, 1200, "keV");
-  E->setBins(100);
-  E->setRange("whole", 0, 1200);
-  E->setRange("betaContinuum", 700, 1200);
-  E->setRange("signalWindow", 420, 600) ;
+  E->setBins(150);
+  E->setRange("whole", E->getMin(), E->getMax());
   
   TFile* f_LYSO = new TFile(lysoFile.c_str());
   TTree* t_LYSO = (TTree*) f_LYSO->Get("tree");
-  RooAddPdf* model = MakeModel(E, t_LYSO);
+  RooDataHist* hist_LYSO = GetDataHistFromTH1(t_LYSO, E, "hE_lyso", "dhE_lyso");
+  cout << "no entries in RooDataHist LYSO = " << hist_LYSO->sum(false) << endl;
+  RooAddPdf* model = MakeModel(E, hist_LYSO);
   
   TFile* f = new TFile(dataFile.c_str());
   TTree* t = (TTree*) f->Get("tree");
   int noEntries = t->GetEntries();
 
-  RooDataHist* hist = GetDataHistFromTH1(t, E, "hE_data", "hist_data");
+  RooDataHist* hist = GetDataHistFromTH1(t, E, "hE_data", "dhE_data");
   cout << "no entries in RooDataHist data = " << hist->sum(false) << endl;
 
+  //E->setRange("betaContinuum", 700, E->getMax());
   RooFitResult* fitRes = model->fitTo(*hist, Extended()); //,Range("betaContinuum"));
-  // Plot unbinned data and histogram pdf overlaid
+  TCanvas* c1 = new TCanvas();
   RooPlot* frame = E->frame(Bins(100)) ;
-  hist->plotOn(frame, DrawOption("PX"));
+  hist->plotOn(frame); //, DrawOption("PX"));
   model->plotOn(frame, Range("whole"));
   model->plotOn(frame, Range("whole"), Components("sig_gaussian"),LineColor(kRed));
   model->plotOn(frame, Range("whole"), Components("histpdf_LYSO"),LineColor(kGreen+2));
-  hist->plotOn(frame) ;
+  hist->plotOn(frame); //, DrawOption("PX"));
   frame->Draw();
   double yShift = 0.07;
   PutText(0.7, 0.85, kBlack, "LAPD");
   PutText(0.7, 0.85-yShift, kBlack, "LPC");
   PutText(0.7, 0.85-2*yShift, kBlack, "Na22 (16 kBq)");
   
-  MakeCalculationsSovSqrtB(model, E, noEntries);
- // TH1* hh_1d = (TH1*) totalPdf->createHistogram("mean,sigma_g2,frac",25,25,25);
+  MakeCalculationsSensitivity(hist_LYSO, model, E, noEntries);
   return 0;
 }
 
